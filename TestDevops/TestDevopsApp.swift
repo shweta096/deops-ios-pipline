98cfff084145f1b03977ae832003bc430a943cc4
F <forushealth@ForusHealths-MacBook-Air.local>
2024-02-15 10:36:17 +0530

    first commit

---
+++ TestDevops/TestDevopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDevopsApp.swift
+//  TestDevops
+//
+//  Created by F on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDevopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
